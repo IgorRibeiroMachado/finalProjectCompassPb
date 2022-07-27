5412f4634971e590dc28c3df8e628eb74181bba7
IgorRibeiroMachado <igoribeirom@gmail.com>
2022-07-27 01:04:02 -0300

    feats: Timer, Temperature, Location

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { map } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -18,11 +21,49 @@ export class HomeComponent implements OnInit {
     min: this.now.getMinutes(),
   }
 
+  public city: any;
+  public state: any;
+  public temp: any;
+  public timer = 60;
+    
+  takeLocation() {
+    const apiKey = 'ac12d8103b6346bf57264097f5692010';
+    navigator.geolocation.getCurrentPosition((position) => {
+      let longitude = position.coords.longitude;
+      let latitude = position.coords.latitude;
+      this.http.get<any>(`http://api.positionstack.com/v1/reverse?access_key=${apiKey}&query=${latitude},${longitude}`).subscribe((valor) => {
+        this.city = valor.data['0'].county;
+        this.state = valor.data['0'].region_code;
+      });    
+    })
+  }
+  
+  takeWeather() {
+    const apiKey = 'cad6085cce14e33a2bbf4128b5268373';
+    navigator.geolocation.getCurrentPosition((position) => {
+      let longitude = position.coords.longitude;
+      let latitude = position.coords.latitude;
+      this.http.get<any>(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`)
+        .subscribe((valor) => {
+          this.temp = Math.trunc(valor.current.temp);
+        })
+    })
+  }
 
-  constructor() { }
+  refreshPage() {
+    setInterval(() => {
+      this.timer--;
+      if (this.timer == 0)
+        this.router.navigate(['/login']);
+    }, 1000)
+  }
+
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
-
+    this.takeLocation();
+    this.takeWeather();
+    this.refreshPage();
   }
 
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,6 +2,7 @@ import { AppRoutingModule } from './../app.routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,6 +11,7 @@ import { HomeComponent } from './home.component';
   imports: [
     CommonModule,
     AppRoutingModule,
+    HttpClientModule
   ],
   exports: [HomeComponent]
 })
